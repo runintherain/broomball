e823b68c8d106f0e2b41aeba770cfa7cee09b3d8
runintherain <atwebb@mtu.edu>
2014-02-09 13:22:36 -0500

    Initial Commit

---
+++ Broomball/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Broomball
+//
+//  Created by Andrew Webb on 2/9/14.
+//  Copyright (c) 2014 WCE Enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BroomballTests/BroomballTests.m
@@ -0,0 +1,34 @@
+//
+//  BroomballTests.m
+//  BroomballTests
+//
+//  Created by Andrew Webb on 2/9/14.
+//  Copyright (c) 2014 WCE Enterprise. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BroomballTests : XCTestCase
+
+@end
+
+@implementation BroomballTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
